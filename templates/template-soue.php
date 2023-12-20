9e9a0a55f2d6214ba506d89ab9f348a56bc8d1ff
oleg.u <oleg.u@thewhitelabelagency.com>
2023-12-20 18:42:11 +0200

    SOUE Template (start)

---
+++ templates/template-soue.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Template Name: SOUE Template
+ */
+?>
+
+<!-- BEGIN  soue-header -->
+<section class="soue-header">
+    <div class="grid-container">
+        <div class="grid-x">
+            <div class="cell">
+
+            </div>
+        </div>
+    </div>
+</section>
+<!-- END  soue-header -->
